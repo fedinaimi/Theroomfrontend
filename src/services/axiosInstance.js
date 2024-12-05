2296fcab83d729b16f4a5f9cbe7802046c1c3552
fedinaimi <fedi.naimi@esprit.tn>
2024-12-05 18:42:20 +0100

    cc

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+// Create an Axios instance using the environment variable for the base URL
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api", // Access the base URL from the .env file
+  baseURL: process.env.REACT_APP_API_BASE_URL, // Use the REACT_APP_API_BASE_URL from the .env file
   headers: {
     'Content-Type': 'application/json',
   },
@@ -9,9 +10,9 @@ const axiosInstance = axios.create({
 
 // Add a request interceptor to include the token in every request
 axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem('token'); // Retrieve the token from localStorage
   if (token) {
-    config.headers['Authorization'] = `Bearer ${token}`;
+    config.headers['Authorization'] = `Bearer ${token}`; // Attach the token to the Authorization header
   }
   return config;
 });
