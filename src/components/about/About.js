11b696a09da5fbdaec27469fe612a13be612b4fd
fedinaimi <fedi.naimi@esprit.tn>
2024-12-20 22:35:51 +0100

    fix-new

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -23,7 +23,9 @@ function About() {
   return (
     <section className="about-section">
       <div className="about-content animate">
-        <h2>À propos de <span className="argent-text">THE ROOM</span></h2>
+        <h2>
+          À propos de <span className="argent-text">THE ROOM</span>
+        </h2>
         <p>
           Bienvenue à The Room! Nous sommes situés au cœur de Menzah 7, où nous
           proposons des expériences uniques sous forme d'escape rooms. Notre
@@ -41,13 +43,26 @@ function About() {
       <div className="about-map animate">
         <h3>Où vous pouvez nous trouver :</h3>
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3201.004015722391!2d10.194071315465232!3d36.84567147993738!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12fd3432b9bff445%3A0x2f01edec7d656f99!2sThe%20Room%20Escape%20Game!5e0!3m2!1sen!2stn!4v1679645251327!5m2!1sen!2stn"
-          width="100%"
-          height="300"
-          style={{ border: "0", borderRadius: "10px" }}
-          allowFullScreen=""
-          loading="lazy"
-        ></iframe>
+  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3200.9994977956575!2d10.1689287!3d36.8536969!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12fd33563fb1afad%3A0xd1e0353f2a6e4df9!2sThe%20Room%20Escape%20Game!5e0!3m2!1sen!2stn!4v1679645251327!5m2!1sen!2stn"
+  width="100%"
+  height="300"
+  style={{ border: "0", borderRadius: "10px" }}
+  allowFullScreen=""
+  loading="lazy"
+></iframe>
+
+        <p style={{ marginTop: "10px" }}>
+          Vous pouvez également nous trouver facilement via Google Maps sur votre
+          téléphone. Cliquez sur ce bouton pour ouvrir directement l'application :
+        </p>
+        <a
+          href="https://maps.app.goo.gl/ChLfGRuS6EiAfofS8"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="map-button"
+        >
+          <i className="fas fa-map-marker-alt"></i> Ouvrir dans Google Maps
+        </a>
       </div>
     </section>
   );
